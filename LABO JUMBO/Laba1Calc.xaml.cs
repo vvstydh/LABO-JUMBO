a4db777b4d92141c488ae25dda1b85dd33695f7a
vvstydh <budkodanila50@gmail.com>
2024-04-29 18:09:36 +0700

    раота с фоном

--- LABO JUMBO/Laba1Calc.xaml.cs
+++ LABO JUMBO/Laba1Calc.xaml.cs
@@ -23,5 +23,15 @@ namespace LABO_JUMBO
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
