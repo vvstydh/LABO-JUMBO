0b2c90f8b185267a8294fd49a051aa2fb4304a15
Кирилл Новиков <novik27.06@mail.ru>
2024-04-30 17:00:18 +0700

    Добавил формулы + валидность + вывод валидности

--- LABO JUMBO/Laba1Calc.xaml.cs
+++ LABO JUMBO/Laba1Calc.xaml.cs
@@ -26,10 +26,27 @@ namespace LABO_JUMBO
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int ugol = int.Parse(Ugol.Text);
-            int dlina = int.Parse(Dlina.Text);
-            int uglkef = int.Parse(Uglkef.Text);
-            Uglusk.Text = (2 * uglkef).ToString();
+            try
+            {
+                double ugol = double.Parse(Ugol.Text) * Math.PI;
+                double dlina = double.Parse(Dlina.Text);
+                double uglkef = double.Parse(Uglkef.Text) * Math.PI;
+                double uglusk = 2 * uglkef;
+                Uglusk.Text = uglusk.ToString();
+                double vrem = Math.Pow(2 * ugol / uglusk, 0.5);
+                Vrem.Text = vrem.ToString();
+                double uglscor = vrem * uglusk;
+                Uglscor.Text = uglscor.ToString();
+                double uskrt = uglusk * dlina;
+                Uskrt.Text = uskrt.ToString();
+                double uskrn = uglscor * uglscor * dlina;
+                Uskrn.Text = uskrn.ToString();
+                double uskro = Math.Pow(Math.Pow(uskrt, 2) + Math.Pow(uskrn, 2), 0.5);
+                Uskro.Text = uskro.ToString();
+                Error.Text = "";
+            }
+            catch (FormatException) { Error.Text = "Неверный формат введенных данных"; }
+            catch (OverflowException) { Error.Text = "Введенные данные слишком большие или слишком малые"; }
         }
     }
 }
